c549216d0cb8910b0216c1b603eae44a085473bc
Jim <51420306+MrYouu@users.noreply.github.com>
2020-11-07 23:41:13 +0200

    Add files via upload

--- Files/Functions.js
+++ Files/Functions.js
@@ -1,1 +1,12 @@
+//  The JavaScript code is property to Mr. You (Kristiyan Valchev) and can only be viewed
+//  If you want to use part of the code, please first contact me: krisvalchev@gmail.com
 
+function buyFormOpen()
+{
+    document.getElementById("buyForm").style.display = "block";
+}
+
+function defaultFunction()
+{
+
+}
